4a2bd8102c67c56f1e576d31f614cb9a7997c6a3
GioAban <gioaban403@gmail.com>
2026-03-13 18:12:36 +0800

    Add .nojekyll and fix config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
-// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/pangasinanheritage",
-  // Subukan nating tanggalin muna ang assetPrefix kung meron ka,
-  // dahil ang basePath ay sapat na para sa karamihan ng GH Pages setup.
   images: {
     unoptimized: true,
   },
